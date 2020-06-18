c111d3aed823060c74445f354d2dd3358921c31e
xiaoji <345865759@163.com>
2020-06-18 22:19:36 +0800

    增加unionfs-fuse脚本

--- reallink/file-local-data-and-files.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-touch time_stun
-#modify to elf of game, so this will start game
-./ELF-file-of-game
-find  . -maxdepth 1 -cnewer time_stun  -type d |grep -v '^\.$' |cut -d/ -f2 > local-data-dires
-find  . -maxdepth 1 -cnewer time_stun  -type f |grep -v '^\.$' |cut -d/ -f2 |grep -v 'local-data-dires' |grep -v 'local-data-files' > local-data-files
-rm time_stun
